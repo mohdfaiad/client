3aa5ea387a95b2e899c538a232bb9b96b5f8154d
Daniel Ayoub <danny.a95@gmail.com>
2018-11-21 13:56:19 -0500

    undo actions changes

--- shared/actions/wallets.js
+++ shared/actions/wallets.js
@@ -23,7 +23,6 @@ import {anyWaiting} from '../constants/waiting'
 import {RPCError} from '../util/errors'
 import {isMobile} from '../constants/platform'
 import {actionHasError} from '../util/container'
-import {debounce} from 'lodash-es'
 
 const buildPayment = (
   state: TypedState,
@@ -84,8 +83,6 @@ const buildPayment = (
     }
   })
 }
-
-const buildPaymentDebounced = debounce(buildPayment, 1000)
 
 const openSendRequestForm = (state: TypedState, action: WalletsGen.OpenSendRequestFormPayload) =>
   state.wallets.acceptedDisclaimer
@@ -727,6 +724,8 @@ function* walletsSaga(): Saga.SagaGenerator<any, any> {
   yield Saga.safeTakeEveryPure(WalletsGen.accountsReceived, maybeSelectDefaultAccount)
   yield Saga.safeTakeEveryPure(WalletsGen.accountsReceived, loadDisplayCurrencyForAccounts)
 
+  // We don't call this for publicMemo/secretNote so the button doesn't
+  // spinner as you type
   yield Saga.actionToPromise(
     [
       WalletsGen.buildPayment,
@@ -738,10 +737,6 @@ function* walletsSaga(): Saga.SagaGenerator<any, any> {
       WalletsGen.displayCurrencyReceived,
     ],
     buildPayment
-  )
-  yield Saga.actionToPromise(
-    [WalletsGen.setBuildingPublicMemo, WalletsGen.setBuildingSecretNote],
-    buildPaymentDebounced
   )
   yield Saga.actionToAction(WalletsGen.openSendRequestForm, openSendRequestForm)
 
